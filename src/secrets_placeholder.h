07bcc784643c7771afd7e664b7f2674e99067ec7
guido57 <guido@dogsally.com>
2026-05-29 09:38:38 +0200

    new

---
+++ src/secrets_placeholder.h
@@ -0,0 +1,11 @@
+// secrets.h
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define SECRET_SSID "WiFi SSID"
+#define SECRET_PASS "WiFi Password"
+
+// You can also store your BSSID here if you are using one
+#define SECRET_BSSID {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF}
+
+#endif
